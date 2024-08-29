e26b3c5394b3f83afe11ec405a2e388999faaf50
connorgalloway <connor_galloway1@baylor.edu>
2024-08-29 15:10:02 -0500

    Inicial committ

---
+++ mygraph.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,20,200)
+plt.plot(x, np.sin(x))
+plt.show()
+
+
+
+#create a virtual environment
+    #python3 -m venv ____
+#activate your VE
+    #source myvenv/bin/activte
+#install 3rd party
+    #pip3 install ____
+
+
+#version control = everything that is related to our project.... videos, logos, documents
+#source control = specifically about the source code.. css and html
---
+++ python1.py
@@ -0,0 +1,12 @@
+a = 1
+b = 2
+c = a + b
+
+for x in range(10):
+    #The red dot means that the code stops before that line is executed
+    y = x * c
+
+
+message = "Hello World!"
+
+print(message)
